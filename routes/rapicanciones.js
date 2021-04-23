5687b7f7e4dff4e9e3ab4bfcfb0dc1ba383180dc
UO246711 <UO246711@uniovi.es>
2021-04-23 10:18:28 +0200

    SDI–2021-316-sesión10-Ejercicio-complementario1

--- routes/rapicanciones.js
+++ routes/rapicanciones.js
@@ -31,71 +31,197 @@ module.exports = function(app, gestorBD) {
     });
 
     app.delete("/api/cancion/:id", function(req, res) {
-        let criterio = { "_id" : gestorBD.mongo.ObjectID(req.params.id)}
+        let criterio = {
+            "_id" : gestorBD.mongo.ObjectID(req.params.id),
+            "autor": app.get("jwt").verify(req.headers.token, 'secreto').usuario
+        }
+
 
         gestorBD.eliminarCancion(criterio,function(canciones){
             if ( canciones == null ){
+
                 res.status(500);
                 res.json({
-                    error : "se ha producido un error"
+                    error:"se ha producido un error"
                 })
             } else {
+
                 res.status(200);
-                res.send( JSON.stringify(canciones) );
+                res.send(JSON.stringify(canciones));
+
             }
         });
     });
 
     app.post("/api/cancion", function(req, res) {
+
+        let errores=[];
+        let generos=["pop","golk","rock","reaggue","rap", "latino","blues","otros"];
+        let nombrec=req.body.nombre;
+        let generoc=req.body.genero;
+        let precioc=req.body.precio;
+
+
+        if(nombrec.trim()=="" || nombrec==null ){
+            errores.push("El titulo de la canción es un campo obligatorio");
+        }
+
+        else{
+
+            if(nombrec.length<3 || nombrec.length>30){
+                errores.push("El nombre de la canción tiene que tener entre 3 y 30 caracteres");
+            }
+
+        }
+        if( generoc.trim()==""||generoc==null ){
+            errores.push("El genero de la canción es un campo obligatorio");
+        }
+
+        else{
+
+            if(!generos.includes(generoc)){
+                errores.push("El genero de la canción no es válido");
+            }
+
+        }
+
+        if( precioc.trim()=="" || precioc==null){
+            errores.push("El precio de la canción es un campo obligatorio");
+        }
+
+        else{
+            if(precioc<0){
+                errores.push("El precio debe ser positivo");
+            }
+
+
+
+        }
+
+
+    if(errores.length==0) {
         let cancion = {
-            nombre : req.body.nombre,
-            genero : req.body.genero,
-            precio : req.body.precio,
+            nombre: req.body.nombre,
+            genero: req.body.genero,
+            precio: req.body.precio,
+            autor: app.get("jwt").verify(req.headers.token, 'secreto').usuario
         }
         // ¿Validar nombre, genero, precio?
 
-        gestorBD.insertarCancion(cancion, function(id){
+
+        gestorBD.insertarCancion(cancion, function (id) {
             if (id == null) {
                 res.status(500);
                 res.json({
-                    error : "se ha producido un error"
+                    error: "se ha producido un error",
+                    errores:errores
+
                 })
             } else {
                 res.status(201);
                 res.json({
-                    mensaje : "canción insertada",
-                    _id : id
+                    errores:errores,
+                    mensaje: "canción insertada",
+                    _id: id
                 })
             }
         });
+    }
+
+    else{
+        res.status(200);
+        res.json({
+            error: "campos no válidos",
+            errores:errores
+
+        })
+
+    }
 
     });
 
     app.put("/api/cancion/:id", function(req, res) {
 
-        let criterio = { "_id" : gestorBD.mongo.ObjectID(req.params.id) };
+        let criterio = {
+            "_id" : gestorBD.mongo.ObjectID(req.params.id) ,
+            "autor": app.get("jwt").verify(req.headers.token, 'secreto').usuario
+        };
 
         let cancion = {}; // Solo los atributos a modificar
-        if ( req.body.nombre != null)
-            cancion.nombre = req.body.nombre;
-        if ( req.body.genero != null)
-            cancion.genero = req.body.genero;
-        if ( req.body.precio != null)
-            cancion.precio = req.body.precio;
-        gestorBD.modificarCancion(criterio, cancion, function(result) {
-            if (result == null) {
-                res.status(500);
-                res.json({
-                    error : "se ha producido un error"
-                })
-            } else {
-                res.status(200);
-                res.json({
-                    mensaje : "canción modificada",
-                    _id : req.params.id
-                })
+
+        let errores=[];
+        let generos=["pop","golk","rock","reaggue","rap", "latino","blues","otros"];
+        let nombrec=req.body.nombre;
+        let generoc=req.body.genero;
+        let precioc=req.body.precio;
+
+
+        if(nombrec.trim()=="" || nombrec==null ){
+            errores.push("El titulo de la canción es un campo obligatorio");
+        }
+
+        else{
+
+            if(nombrec.length<3 || nombrec.length>30){
+                errores.push("El nombre de la canción tiene que tener entre 3 y 30 caracteres");
             }
-        });
+
+        }
+
+        if( generoc.trim()==""||generoc==null ){
+            errores.push("El genero de la canción es un campo obligatorio");
+        }
+
+        else{
+
+            if(!generos.includes(generoc)){
+                errores.push("El genero de la canción no es válido");
+            }
+
+        }
+        if( precioc.trim()=="" || precioc==null){
+            errores.push("El precio de la canción es un campo obligatorio");
+        }
+
+        else{
+            if(precioc<0){
+                errores.push("El precio debe ser positivo");
+            }
+
+
+        }
+
+        if(errores.length==0) {
+            cancion.nombre = req.body.nombre;
+            cancion.genero = req.body.genero;
+            cancion.precio = req.body.precio;
+
+
+            gestorBD.modificarCancion(criterio, cancion, function (result) {
+                if (result == null) {
+                    res.status(500);
+                    res.json({
+                        error: "se ha producido un error"
+                    })
+                } else {
+                    res.status(200);
+                    res.json({
+                        mensaje: "canción modificada",
+                        _id: req.params.id
+                    })
+                }
+            });
+        }
+
+        else{
+            res.status(200);
+            res.json({
+                error: "campos no válidos",
+                errores:errores
+
+            })
+
+        }
     });
 
 
